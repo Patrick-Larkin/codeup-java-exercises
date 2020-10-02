c6c052c5c49f59e0241c404e138f23b00cb31e98
patrick <patricklarkin995@gmail.com>
2020-10-02 09:07:18 -0500

    finished with exercises, end of day

--- src/ControlStructuresLec.java
+++ src/ControlStructuresLec.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ControlStructuresLec {
     public static void main(String[] args) {
 //        System.out.println("Control Structure Lec");
-//        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 //
 //        //Get Employee Id?
 //        System.out.print("What is your employee ID?");
@@ -120,10 +120,91 @@ public class ControlStructuresLec {
 //            System.out.println("A = 8 Break!");
 //        }
 
+
+// Exercises
+//While
         int i = 5;
         while (i <= 15) {
             System.out.println(i);
             i++;
         }
+
+//Do while #1
+        int a = 0;
+        do {
+            a = a + 2;
+            System.out.println("Do while loop counter A: " + a);
+        } while (a < 100);
+
+//Do while #2
+        int b = 100;
+        do {
+
+            System.out.println("Do while loop counter B: " + b);
+            b = b - 5;
+        } while (b > -10);
+
+//Do while #3
+        long c = 2;
+        do {
+            System.out.println("Do while loop counter C: " + c);
+            c *= c;
+        } while (c < 1000000L);
+
+//For loop #1
+        for (int d = 100; d > -10; d-=5) {
+            System.out.println("For loop counter D: " + d);
+        }
+//For loop #2
+        for (long e = 2; e <= 10000; e*=e) {
+            System.out.println("For loop counter E: " + e);
+        }
+//Fizz Buzz
+        for (int f = 0; f <= 100; f++) {
+            if (f % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (f % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (f % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(f);
+            }
+        }
+//Table of Powers
+        System.out.println("Enter a number");
+        int userNumber = Integer.parseInt(sc.next());
+        System.out.println(" number | squared | cubed ");
+        System.out.println("------- | ------- | ------");
+        for (int h = 0; h < userNumber; h++) {
+            int squared = h * h;
+            int cubed = h * h * h;
+            System.out.printf("%-6d\t", h);
+            System.out.print(" | ");
+            System.out.printf("%-6d", squared);
+            System.out.print(" | ");
+            System.out.printf("%-6d", cubed);
+            System.out.println();
+        }
+
+//Grades to Numbers
+        String userContinue;
+        do {
+            System.out.println("Enter your Grade");
+            int grade = Integer.parseInt(sc.next());
+            if (grade >= 88) {
+                System.out.println("A");
+            } else if (grade >= 80) {
+                System.out.println("B");
+            } else if (grade >= 67) {
+                System.out.println("C");
+            } else if (grade >= 60) {
+                System.out.println("D");
+            } else {
+                System.out.println("F");
+            }
+            System.out.println("Do you wish to continue?(y/n)");
+            userContinue = sc.next();
+        } while (userContinue.equals('y'));
     }
 }
